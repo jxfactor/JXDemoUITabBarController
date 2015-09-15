a2f6dfbaa4230f7b2e22e13ef4cd9520054888b8
jxfactor <kingjun15@126.com>
2015-09-15 12:02:07 +0800

    Initial Commit

---
+++ JXDemoUITabBarController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JXDemoUITabBarController
+//
+//  Created by 汪骏祥 on 9/15/15.
+//  Copyright (c) 2015 junxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JXDemoUITabBarController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JXDemoUITabBarController
+//
+//  Created by 汪骏祥 on 9/15/15.
+//  Copyright (c) 2015 junxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
